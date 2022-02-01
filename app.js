1b846788396d8b1d195ed050c75a3cf5da293e1e
Jose Enrique Martinez <jmart2v@ar.la.holcim.net>
2022-01-31 23:23:50 -0300

    nuevaPrueba

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const publicPath = path.resolve(__dirname,'./public');
 app.use(express.static(publicPath));
 
 // Cambiar bloque para Heroku
-app.listen(process.env.PORT || 3000, () => console.log("server start"));
+app.listen(process.env.PORT || 3000, function() {console.log("server start")});
 
 app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"));
